e6cfc50b5c3c7f3054308c3238525252843b2cdb
Repyh <63894915+repyh@users.noreply.github.com>
2021-09-08 09:51:09 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,4 @@
-const form = document.querySelector('form');
-const items = document.getElementById('items');
-const userID = document.getElementById('inputUser');
 const save = document.getElementById('save-id');
-const host = window.location.protocol + '//' + window.location.host;
 
 window.onload = () => {
     if(localStorage.getItem('user_id')) {
@@ -10,15 +6,3 @@ window.onload = () => {
         save.checked = true;
     }
 }
-
-form.addEventListener('submit', async e => {
-    e.preventDefault();
-    fetch(`${host}/checkout?user_id=${userID.value}&item=${items.value.split('-')[0].trim()}`, {
-        method: 'POST'
-    }).then(async res => {
-        if(save.checked) localStorage.setItem('user_id', userID.value);
-        else localStorage.removeItem('user_id');
-        const response = await res.json();
-        return window.location.replace(response.redirect);
-    })
-})
